4ab3398365f44359b33aabea06aac5f64c275bf9
Wafeek Olatunji <wafeek.olatunji@grano.fi>
2020-07-22 21:25:52 +0300

    Chore: Remove redundant docker config files

--- docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t wafeekolatunji/multi-client ./client
-docker tag multi-client wafeekolatunji/multi-client
-docker build -t wafeekolatunji/multi-nginx ./nginx
-docker build -t wafeekolatunji/multi-server ./server
-docker build -t wafeekolatunji/multi-worker ./worker
